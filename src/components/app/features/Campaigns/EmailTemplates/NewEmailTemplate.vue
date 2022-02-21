faaaa2d16134236a85ccf66c08517f8c6a5bce57
Jonathan Ijeh <captajo2@gmail.com>
2022-02-21 13:31:33 +0100

    Addition of html editor to vue package

--- src/components/app/features/Campaigns/EmailTemplates/NewEmailTemplate.vue
+++ src/components/app/features/Campaigns/EmailTemplates/NewEmailTemplate.vue
@@ -20,18 +20,64 @@
     </div>
     <v-container>
       <v-row>
-        <!-- New Email Template Area -->
+        <!-- <div class="text-editor-container"> -->
+          <div id="bar">
+            <h1>Vue Email Editor (Demo)</h1>
+
+            <button class="gradient curved white--text text-capitalize v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default" v-on:click="saveDesign">Save Design</button>
+            <button class="gradient curved white--text text-capitalize v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default" v-on:click="exportHtml">Export HTML</button>
+          </div>
+          <!-- New Email Template Area -->
+          <EmailEditor
+            ref="emailEditor"
+            v-on:load="editorLoaded"
+            v-on:ready="editorReady"
+          />
+        <!-- </div> -->
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {
+import { EmailEditor } from 'vue-email-editor';
 
+export default {
+  components: {
+    EmailEditor,
+  },
+  mounted() {
+  },
+  destroyed() {
+    
+  },
+  methods: {
+    // called when the editor is created
+    editorLoaded() {
+      console.log('editorLoaded');
+      // Pass the template JSON here
+      // this.$refs.emailEditor.editor.loadDesign({});
+    },
+    // called when the editor has finished loading
+    editorReady() {
+      console.log('editorReady');
+    },
+    saveDesign() {
+      this.$refs.emailEditor.editor.saveDesign((design) => {
+        console.log('saveDesign', design);
+      });
+    },
+    exportHtml() {
+      this.$refs.emailEditor.editor.exportHtml((data) => {
+        console.log('exportHtml', data);
+      });
+    },
+  }
 }
 </script>
 
 <style>
-
+.text-editor-container {
+  padding: 10px;
+}
 </style>
