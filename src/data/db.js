469cbc5ed449b63f07adf3abd3a3789eb96d998d
Bankole <ebankole@aidml.com>
2022-03-11 20:27:33 +0100

    more apis

--- src/data/db.js
+++ src/data/db.js
@@ -1,11 +1,20 @@
 const rawContacts = require('./mock_contacts.json');
+const account = require('./account.json');
 const cards = require('./mock_cards.json')
+const campaigns = require('./campaigns.json');
 const segments = require('./Segments.json');
 const categories = require('./listCategory.json')
 const lists = require('./contactLists.json')
 const labels = require('./labels.json')
 const eventTypes = require('./tracker_event_types.json');
 const events = require('./tracker_events.json');
+const emailSenders = require('./mock_email_senders.json');
+const smsSenders = require('./mock_sms_senders.json');
+const emailTemplates = require('./email_templates.json');
+const emailTemplateSamples = require('./email_template_samples.json');
+const trackerGoals = require('./tracker_goals.json');
+const trackerEvents = require('./tracker_events.json');
+const transactions = require('./transactions.json');
 // console.log({contacts, cards, segments});
 
 module.exports = () => {
@@ -21,6 +30,24 @@ module.exports = () => {
     // }
     return contact;
   })
-  const data = { contacts, cards, segments, categories, lists, labels, eventTypes, events }
+  const data = { 
+    account,
+    contacts, 
+    cards, 
+    segments, 
+    categories, 
+    lists, 
+    labels, 
+    eventTypes, 
+    events, 
+    emailSenders, 
+    smsSenders, 
+    emailTemplates, 
+    emailTemplateSamples, 
+    trackerGoals, 
+    trackerEvents, 
+    transactions,
+    campaigns
+  }
   return data
 }
